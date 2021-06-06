391c67f75d2940540a3cb9f93b5abd00f9e4f7a7
Sam <shonor47@gmail.com>
2021-06-06 17:49:45 -0400

    Boilerplate TeleOp code; Motors declared and initialized

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class TeleOp extends OpMode {
+    private DcMotor frontRight, frontLeft, backRight, backLeft;
+
+    @Override
+    public void init() {
+        frontRight = hardwareMap.get(DcMotor.class, "fr");
+        frontLeft = hardwareMap.get(DcMotor.class, "fl");
+        backRight = hardwareMap.get(DcMotor.class, "br");
+        backLeft = hardwareMap.get(DcMotor.class, "bl");
+    }
+
+    @Override
+    public void loop() {
+        
+    }
+}
